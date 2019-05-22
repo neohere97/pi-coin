cb262945250963b20df46350ed2d07bb4b5123d6
neohere97 <shalawadi.chinmay@gmail.com>
2019-05-22 23:49:46 +0530

    d

--- src/generate_pooler.py
+++ src/generate_pooler.py
@@ -100,7 +100,7 @@ def send_to_pooler(transaction):
         threading.Thread(target=monitor_distribute).start()
         
 def announce_node_to_network():
-    time.sleep(1)
+    time.sleep(5)
     global peer_nodes
     http = httplib2.Http()
     for i in peer_nodes:        
